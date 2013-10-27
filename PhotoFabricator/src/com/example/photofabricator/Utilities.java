fe829a2860449601981692a0e574f8da005d85a4
soliloquial <weiss.spencer@gmail.com>
2013-10-26 21:16:25 -0400

    Added HTTP upload

--- PhotoFabricator/src/com/example/photofabricator/Utilities.java
+++ PhotoFabricator/src/com/example/photofabricator/Utilities.java
@@ -1,9 +1,14 @@
 package com.example.photofabricator;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -11,6 +16,7 @@ import org.jibble.simpleftp.SimpleFTP;
 
 import android.graphics.Bitmap;
 import android.os.Environment;
+import android.widget.Toast;
 
 public class Utilities {
 
@@ -18,7 +24,78 @@ public class Utilities {
 	private static final String FTP_USERNAME = "anonymous";
 	private static final String FTP_PASSWORD = "";
 
-	public static void uploadFile(String path) {
+	
+	public static void uploadViaHTTP(String path) {
+		HttpURLConnection connection = null;
+		DataOutputStream outputStream = null;
+		DataInputStream inputStream = null;
+
+		String pathToOurFile = path;
+		String urlServer = "http://192.168.1.73/";
+		String lineEnd = "\r\n";
+		String twoHyphens = "--";
+		String boundary =  "*****";
+
+		int bytesRead, bytesAvailable, bufferSize;
+		byte[] buffer;
+		int maxBufferSize = 1*1024*1024;
+
+		try
+		{
+		FileInputStream fileInputStream = new FileInputStream(new File(pathToOurFile) );
+
+		URL url = new URL(urlServer);
+		connection = (HttpURLConnection) url.openConnection();
+
+		// Allow Inputs & Outputs
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+		connection.setUseCaches(false);
+
+		// Enable POST method
+		connection.setRequestMethod("POST");
+
+		connection.setRequestProperty("Connection", "Keep-Alive");
+		connection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
+
+		outputStream = new DataOutputStream( connection.getOutputStream() );
+		outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+		outputStream.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + pathToOurFile +"\"" + lineEnd);
+		outputStream.writeBytes(lineEnd);
+
+		bytesAvailable = fileInputStream.available();
+		bufferSize = Math.min(bytesAvailable, maxBufferSize);
+		buffer = new byte[bufferSize];
+
+		// Read file
+		bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+		while (bytesRead > 0)
+		{
+		outputStream.write(buffer, 0, bufferSize);
+		bytesAvailable = fileInputStream.available();
+		bufferSize = Math.min(bytesAvailable, maxBufferSize);
+		bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+		}
+
+		outputStream.writeBytes(lineEnd);
+		outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+		// Responses from the server (code and message)
+		int serverResponseCode = connection.getResponseCode();
+		String serverResponseMessage = connection.getResponseMessage();
+
+		fileInputStream.close();
+		outputStream.flush();
+		outputStream.close();
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+	public static void uploadViaFTP(String path) {
 		SimpleFTP ftp = new SimpleFTP();
 		try {
 			ftp.connect(FTP_SERVER, 21, FTP_USERNAME, FTP_PASSWORD);
@@ -28,6 +105,9 @@ public class Utilities {
 			ftp.disconnect();
 		} catch (IOException e) {
 		}
+	}
+	public static void uploadFile(String path) {
+		uploadViaHTTP(path);
 	}
 	
 	public static String makeFilename(String prefix) {
